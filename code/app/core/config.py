d3938483eaf7ac4617cf99bda3fed111c488d757
Carlo1911 <carlo.alva@pucp.pe>
2021-11-30 19:38:08 -0500

    [CHANGE] remove app

--- code/app/core/config.py
+++ code/app/core/config.py
@@ -1,15 +1,15 @@
-from typing import Union
+from typing import List, Union
 
 from pydantic import AnyHttpUrl, BaseSettings, validator
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-    BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
     # TODO: Add more settings here (AWS, DynamoDB, etc.)
 
     @validator('BACKEND_CORS_ORIGINS', pre=True)
-    def assemble_cors_origins(cls, v: Union[str, list[str]]) -> Union[list[str], str]:
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
         print(f'v: {v}')
         if isinstance(v, str) and not v.startswith('['):
             return [i.strip() for i in v.split(',')]
