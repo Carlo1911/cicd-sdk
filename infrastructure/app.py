f8233103682983a2bbebf24bc34a74c83159ac02
Carlo1911 <carlo.alva@pucp.pe>
2021-12-09 14:24:01 -0500

    [CHANGE] port

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import os
+
 import aws_cdk as cdk
 
-from infrastructure.infrastructure_stack import AuthUserServiceStack
 from infrastructure.config import get_config
+from infrastructure.infrastructure_stack import AuthUserServiceStack
 
-env_name = os.environ.get('ENV_NAME', 'Prod')
+env_name = os.environ.get("ENV_NAME", "Prod")
+app_name = f"{env_name}AuthUserService"
 
 app = cdk.App()
 config = get_config(deploy_name=env_name)
-AuthUserServiceStack(app, f"{env_name}AuthUserService", app_name=f"{env_name}AuthUserService", config=config)
+AuthUserServiceStack(app, app_name, app_name=app_name, config=config)
 
 app.synth()
