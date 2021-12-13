6369b5eede0ae2141dc4a43eebfd4eab4e603473
Carlo1911 <carlo.alva@pucp.pe>
2021-12-13 10:45:34 -0500

    [ADD] test boto3

--- code/app/users/endpoints.py
+++ code/app/users/endpoints.py
@@ -13,6 +13,7 @@ dynamodb = boto3.resource(
     region_name="us-west-2",
     aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.environ.get("AWS_SESSION_TOKEN"),
 )
 
 
