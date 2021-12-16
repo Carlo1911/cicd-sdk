fe418e153da96ea4e855b9606948298073dc19ed
Carlo1911 <carlo.alva@pucp.pe>
2021-12-16 08:08:37 -0500

    [ADD] tests

--- code/src/users/tests.py
+++ code/src/users/tests.py
@@ -46,12 +46,39 @@ def test_create_user(user):
 
 
 def test_update_user(user):
+    update_user = {
+        "UID": "45078f7613bf456a8b62497aa3febd83",
+        "MiddleName": "Andre",
+        "UpdatedAt": "2021-11-19",
+        "DOB": "1991-11-19",
+        "FirstName": "Carlo",
+        "SSN": "123-11-1234",
+        "Addresses": {
+            "Mailing": {
+                "City": "New York",
+                "StreetLine2": "Apt A",
+                "State": "NY",
+                "ZipCode": "12345",
+                "StreetLine1": "123 Street",
+            },
+            "Billing": {
+                "City": "New York",
+                "StreetLine2": "Apt A",
+                "State": "NY",
+                "ZipCode": "12345",
+                "StreetLine1": "123 Street",
+            },
+        },
+        "PhoneNumbers": {"Home": "4448969", "Cell": "0987654321"},
+        "LastName": "Cohello",
+        "CreatedAt": "2021-11-21",
+    }
     response = client.patch(
         f"/users/{user['UID']}",
-        json=user,
+        json=update_user,
     )
     assert response.status_code == 200
-    assert response.json() == user
+    assert response.json() == update_user
 
 
 def test_get_user(user):
